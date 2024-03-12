453d239dc4beff6d8dd5be0a31d34e39e243ac7f
Mzzqq <rezarifa661@gmail.com>
2024-03-12 16:48:52 +0700

    feat: Add function for converting hundreds to text

--- money-to-text.go
+++ money-to-text.go
@@ -3,3 +3,29 @@ package money_text
 var units = []string{"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"}
 var teens = []string{"sepuluh", "sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", "enam belas", "tujuh belas", "delapan belas", "sembilan belas"}
 var tens = []string{"", "sepuluh", "dua puluh", "tiga puluh", "empat puluh", "lima puluh", "enam puluh", "tujuh puluh", "delapan puluh", "sembilan puluh"}
+
+func konversiRatusan(n int) string {
+	str := ""
+	if n >= 100 {
+		str += units[n/100] + " ratus"
+		n %= 100
+		if n > 0 {
+			str += " "
+		}
+	}
+	if n >= 20 {
+		str += tens[n/10]
+		n %= 10
+		if n > 0 {
+			str += " "
+		}
+	}
+	if n >= 10 {
+		str += teens[n-10]
+		n = 0
+	}
+	if n > 0 {
+		str += units[n]
+	}
+	return str
+}
