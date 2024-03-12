676e13418e7a31ed02bc9635bd08e3b9eda2ebeb
Mzzqq <rezarifa661@gmail.com>
2024-03-12 16:47:23 +0700

    feat: Add slices for representing numbers

--- money-to-text.go
+++ money-to-text.go
@@ -1,1 +1,5 @@
 package money_text
+
+var units = []string{"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"}
+var teens = []string{"sepuluh", "sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", "enam belas", "tujuh belas", "delapan belas", "sembilan belas"}
+var tens = []string{"", "sepuluh", "dua puluh", "tiga puluh", "empat puluh", "lima puluh", "enam puluh", "tujuh puluh", "delapan puluh", "sembilan puluh"}
